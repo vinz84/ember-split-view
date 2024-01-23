e540b96e613382fae1d30d7119a1d45cb0d20f28
Emanuel Vinzent <emanuel.vinzent@sichtwerk.com>
2024-01-23 10:10:35 +0100

    Upgrade Ember Global

--- addon/components/split-view.js
+++ addon/components/split-view.js
@@ -6,6 +6,7 @@ import { A } from '@ember/array';
 import { next, scheduleOnce } from '@ember/runloop'
 import SplitChild from './split-child';
 import splitViewLayout from 'ember-split-view/templates/components/split-view';
+import Component from '@ember/component';
 
 
 /**

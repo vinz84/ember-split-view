b40a3885b36577d7c735a1e37439e5ca9db0c018
Emanuel Vinzent <emanuel.vinzent@sichtwerk.com>
2024-01-24 17:23:19 +0100

    Remove css

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name,
-  included: function(app) {
-    this._super.included(app);
-    app.import('vendor/ember-split-view/styles/split-view.css', { destDir: '/' });
-  }
+  name: require('./package').name
+  
 };
